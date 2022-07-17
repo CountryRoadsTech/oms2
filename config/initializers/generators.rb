367bc53c8241df35b279fa3374d4ae6e55fe9e02
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2022-07-17 10:24:56 -0400

    Enable Postgres UUID database key type and set as the default primary key type in generators

---
+++ config/initializers/generators.rb
@@ -0,0 +1,4 @@
+# Sets UUIDs as the default primary key type in generated database migrations
+Rails.application.config.generators do |g|
+  g.orm :active_record, primary_key_type: :uuid
+end
---
+++ db/migrate/20220717142101_enable_postgres_uuids.rb
@@ -0,0 +1,6 @@
+# Required to use UUID as a key type in the database
+class EnablePostgresUuids < ActiveRecord::Migration[7.0]
+  def change
+    enable_extension('pgcrypto') unless extension_enabled?('pgcrypto')
+  end
+end
