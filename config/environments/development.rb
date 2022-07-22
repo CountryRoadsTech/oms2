8700aec4b2ee666e6c1d81176b862b3630815607
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2022-07-22 17:08:22 -0400

    Display stylesheets inline, and update rich text fields in user authorization views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,6 +72,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.sass.inline_source_maps = true
+
   # Enable Bullet detection of N+1 database queries
   config.after_initialize do
     Bullet.enable = true
